Fix an issue masking output in the attention mechanism

audio_to_midi_dataset.py
@@ -330,7 +330,7 @@ def audio_to_midi_dataset_generator(
         event_indices = jnp.arange(selected_midi_events.shape[1])
         seen_event_mask = event_indices < picked_midi_splits[:, jnp.newaxis]
         seen_events = selected_midi_events.at[~seen_event_mask].set(
-            jnp.array([BLANK_MIDI_EVENT, 0])
+            jnp.array([0, BLANK_MIDI_EVENT])
         )
 
         # We can get rid of events that are BLANK_MIDI_EVENT for all samples in the batch
@@ -373,7 +373,7 @@ class AudioToMidiDatasetLoader:
                 events,
                 ((0, max_events_length - events.shape[0])),
                 "constant",
-                constant_values=(BLANK_MIDI_EVENT, 0),
+                constant_values=(0, BLANK_MIDI_EVENT),
             )
             for events in unpadded_midi_events
         ]

infer.py
@@ -71,13 +71,11 @@ def main():
     print("Infering midi events...")
     last_event = (0, SEQUENCE_START)
     seen_events = jnp.array([last_event])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
-    # seen_events = jnp.vstack([seen_events, jnp.array([-1, 0])])
+
+    # Inference result should not change appending padding events!
+    # seen_events = jnp.vstack([seen_events, jnp.array([0, -1])])
+    # seen_events = jnp.vstack([seen_events, jnp.array([0, -1])])
+    # seen_events = jnp.vstack([seen_events, jnp.array([0, -1])])
 
     infer_limit = 50
     i = 0

model.py
@@ -223,6 +223,7 @@ class AttentionBlock(eqx.Module):
                 encoder_output.shape[0]
                 if encoder_output is not None
                 else inputs.shape[0],
+                encoder_output is None,
             )
 
         attention_key, dropout_key = jax.random.split(key)
@@ -249,14 +250,18 @@ class AttentionBlock(eqx.Module):
         self,
         input_mask: Integer[Array, " q_seq"],
         key_value_sequence_length: int,
+        # TODO: Consider a nicer way of doing this
+        mask_key_values: bool,  # If inputs are used as kv sequences, we need to mask those too!
     ) -> Float[Array, "q_seq kv_seq"]:
         """Create self-attention mask from sequence-level mask."""
 
+        kv_mask = jnp.ones(key_value_sequence_length, dtype=jnp.int32)
+        if mask_key_values:
+            kv_mask = input_mask
+
         mask = jnp.multiply(
             jnp.expand_dims(input_mask, axis=-1),
-            jnp.expand_dims(
-                jnp.ones(key_value_sequence_length, dtype=jnp.int32), axis=-2
-            ),
+            jnp.expand_dims(kv_mask, axis=-2),
         )
         return mask.astype(jnp.bool_)
 
@@ -574,7 +579,7 @@ class OutputSequenceGenerator(eqx.Module):
         mask = jnp.asarray(generated_output[:, 1] != BLANK_MIDI_EVENT, dtype=jnp.int32)
 
         midi_logits, midi_probs, position_logits, position_probs = self.decoder(
-            decoder_output=midi_embeddings_so_far,  # generate midi embeddings
+            decoder_output=midi_embeddings_so_far,
             encoder_output=encoder_output,
             mask=mask,
             enable_dropout=enable_dropout,