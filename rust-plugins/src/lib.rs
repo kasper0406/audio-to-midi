2937397743902fc7e4b707e96c40b3eafb3685fe
Kasper Nielsen <kasper0406@gmail.com>
2024-03-22 16:38:31 +0100

    Construct and return numpy arrays

--- rust-plugins/src/lib.rs
+++ rust-plugins/src/lib.rs
@@ -1,4 +1,5 @@
 use numpy::PyArray1;
+use numpy::PyArray2;
 use numpy::ToPyArray;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
@@ -18,17 +19,19 @@ static TOKIO_RUNTIME: Lazy<Runtime> = Lazy::new(|| {
 
 #[derive(Debug, Deserialize)]
 struct EventRecord {
-    time: f64,
-    duration: f64,
+    time: f32,
+    duration: f32,
     key: u32,
-    velocity: f64,
+    velocity: f32,
 }
 
-fn key_to_event(key: u32) -> u32 {
-    2 + (key - 21)
+fn key_to_event(key: u32) -> i32 {
+    2 + (key - 21) as i32
 }
 
-async fn process_file(path: &str, max_event_time: f64) -> Result<String, std::io::Error> {
+type MidiEvents = Vec<(f32, i32, f32)>;
+
+async fn process_file(path: &str, max_event_time: f32) -> Result<MidiEvents, std::io::Error> {
     let mut file = File::open(path).await?;
     let mut contents = String::new();
     file.read_to_string(&mut contents).await?;
@@ -55,45 +58,71 @@ async fn process_file(path: &str, max_event_time: f64) -> Result<String, std::io
             Err(e) => eprintln!("Failed to deserialize record: {:?}", e),
         }
     }
+    events.sort_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Greater));
 
-    println!("Calculated events: {:?}", events);
-
-    Ok(contents)
+    let mut events_with_padding: MidiEvents = vec![];
+    events_with_padding.push((0.0, 0, 0.0));
+    events_with_padding.append(&mut events);
+    events_with_padding.push((0.0, -1, 0.0));
+    Ok(events_with_padding)
 }
 
 #[pyfunction]
-fn events_from_samples(py: Python, dataset_dir: String, sample_names: &PyList, max_event_time: f64) -> PyResult<Py<PyArray1<f64>>> {
-    TOKIO_RUNTIME.block_on(async {
-        let mut join_set = JoinSet::new();
-
-        for maybe_sample_name in sample_names {
-            match maybe_sample_name.extract::<String>() {
-                Ok(sample_name) => {
-                    let sample_csv_file = format!("{}/{}.csv", dataset_dir, sample_name);
-                    join_set.spawn(async move { process_file(&sample_csv_file, max_event_time).await });
-                },
-                Err(_) => {
-                    return Err(PyTypeError::new_err("Sample names must be a list of strings!"));
-                }
+fn events_from_samples(py: Python, dataset_dir: String, sample_names: &PyList, max_event_time: f32) -> PyResult<Py<PyList>> {
+    let mut sample_files = vec![];
+    for maybe_sample_name in sample_names {
+        match maybe_sample_name.extract::<String>() {
+            Ok(sample_name) => {
+                let sample_csv_file = format!("{}/{}.csv", dataset_dir, sample_name);
+                sample_files.push(sample_csv_file);
+            },
+            Err(_) => {
+                return Err(PyTypeError::new_err("Sample names must be a list of strings!"));
             }
         }
+    }
 
-        while let Some(result) = join_set.join_next().await {
-            match result {
-                Ok(Ok(content)) => {
-                    println!("Read file content with length: {}", content.len());
-                },
-                Ok(Err(err)) => {
-                    return Err(PyTypeError::new_err(format!("Failed to read sample: {:?}", err)))
-                },
-                Err(err) => {
-                    return Err(PyTypeError::new_err(format!("Error during processing: {:?}", err)))
+    let all_events = py.allow_threads(move || {
+        TOKIO_RUNTIME.block_on(async {
+            let mut join_set = JoinSet::new();
+
+            for sample_file in sample_files {
+                join_set.spawn(async move { process_file(&sample_file, max_event_time).await });
+            }
+
+            let mut all_events = vec![];
+            while let Some(result) = join_set.join_next().await {
+                match result {
+                    Ok(Ok(events)) => {
+                        all_events.push(events);
+                    },
+                    Ok(Err(err)) => {
+                        return Err(PyTypeError::new_err(format!("Failed to read sample: {:?}", err)))
+                    },
+                    Err(err) => {
+                        return Err(PyTypeError::new_err(format!("Error during processing: {:?}", err)))
+                    }
                 }
             }
-        }
+            Ok(all_events)
+        })
+    }).unwrap();
 
-        Ok(PyArray1::<f64>::zeros(py, 10, false).into())
-    })
+    let mut numpy_results = vec![];
+    for events in all_events {
+        let mut np_array = PyArray2::<f32>::zeros(py, [events.len(), 3], false);
+        {
+            let mut array_read_write = np_array.readwrite();
+            for (row, (time, key, velocity)) in events.into_iter().enumerate() {
+                *array_read_write.get_mut([row, 0]).unwrap() = time as f32;
+                *array_read_write.get_mut([row, 1]).unwrap() = key as f32;
+                *array_read_write.get_mut([row, 2]).unwrap() = velocity as f32;
+            }
+        }
+        numpy_results.push(np_array);
+    }
+
+    Ok(PyList::new(py, &numpy_results).into())
 }
 
 #[pymodule]
