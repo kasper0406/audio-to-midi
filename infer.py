Fix loss calculation

infer.py
@@ -146,13 +146,13 @@ def batch_infer(
         durations = jnp.select(
             [end_of_sequence_mask],
             [jnp.zeros((batch_size,), jnp.int16)],
-            jnp.round(raw_durations[0] * frames.shape[0]),
+            jnp.round(jnp.sum(raw_durations, axis=-1) * frames.shape[0]),
         )
 
         velocities = jnp.select(
             [end_of_sequence_mask],
             [jnp.zeros((batch_size,), jnp.int16)],
-            jnp.minimum(NUM_VELOCITY_CATEGORIES, jnp.round(raw_velocities[0] * NUM_VELOCITY_CATEGORIES)),
+            jnp.minimum(NUM_VELOCITY_CATEGORIES, jnp.round(jnp.sum(raw_velocities, axis=-1) * NUM_VELOCITY_CATEGORIES)),
         )
 
         # Combine the predicted positions with their corresponding midi events

train.py
@@ -170,7 +170,8 @@ def compute_test_loss(
     losses = jnp.select([actual_event_mask], [losses], 0.0)
     individual_losses = jnp.select([actual_event_mask], [individual_losses], 0.0)
 
-    return jnp.sum(losses) / jnp.count_nonzero(actual_event_mask), jnp.mean(individual_losses, axis=1)
+    num_events = jnp.count_nonzero(actual_event_mask)
+    return jnp.sum(losses) / num_events, jnp.sum(individual_losses, axis=1) / num_events
 
 
 def compute_testset_loss(model, testset_dir: Path, key: jax.random.PRNGKey, sharding, batch_size=32):